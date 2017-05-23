f1ca8effa64dab1d3b1d91647a7c09003140d955
TheCtac <thectac0211@gmail.com>
2017-05-23 12:13:13 +0300

    some changes

--- src/Acme/TanksBundle/Controller/TanksController.php
+++ src/Acme/TanksBundle/Controller/TanksController.php
@@ -257,7 +257,6 @@ class TanksController extends Controller
 		}
 		$repository =$this->getDoctrine()->getRepository('AcmeTanksBundle:users');
 		$user=$repository->findOneByHash($hash);
-		
 		return $this->render('AcmeTanksBundle::userRed.html.twig',array('user'=>$user));				
 	}
     public function userMessAction($type){
